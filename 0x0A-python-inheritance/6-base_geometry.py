99ffb8d6fd72209de2ea98da4d24502360657070
lebolesia <lebohanglesia1999@gmail.com>
2024-01-08 11:24:34 -0800

    6-base_geometry.py

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Describes base geometry class BaseGeometry."""
+
+
+class BaseGeometry:
+    """Symbolizes base geometry."""
+
+    def area(self):
+        """Not applied."""
+        raise Exception("area() is not applied")
